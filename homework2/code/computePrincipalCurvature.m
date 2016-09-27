2297d6fd5b0e2f5088b4df5cace3429992467fb9
Luis <siquike@gmail.com>
2016-09-27 12:14:13 -0400

    Started homework 2

---
+++ homework2/code/computePrincipalCurvature.m
@@ -0,0 +1,8 @@
+function PrincipalCurvature = computePrincipalCurvature(DoGPyramid)
+%%Edge Suppression
+% inputs
+% DoG Pyramid - size (size(im), numel(levels) - 1) matrix of the DoG pyramid
+%
+% outputs
+% PrincipalCurvature - size (size(im), numel(levels) - 1) matrix where each point contains the curvature ratio R for the 
+% 					   corresponding point in the DoG pyramid
---
+++ homework2/code/getLocalExtrema.m
@@ -0,0 +1,10 @@
+function locs = getLocalExtrema(DoGPyramid, DoGLevels, PrincipalCurvature,th_contrast, th_r)
+%%Detecting Extrema
+% inputs
+% DoG Pyramid - size (size(im), numel(levels) - 1) matrix of the DoG pyramid
+% DoG Levels
+% PrincipalCurvature - size (size(im), numel(levels) - 1) matrix contains the curvature ratio R
+% th_contrast - remove any point that is a local extremum but does not have a DoG response magnitude above this threshold
+% th_r - remove any edge-like points that have too large a principal curvature ratio
+% output
+% locs - N x 3 matrix where the DoG pyramid achieves a local extrema in both scale and space, and also satisfies the two thresholds.
