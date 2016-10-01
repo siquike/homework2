b0429c9a95d094a54c1ebb039941cf55067d1a89
Luis <siquike@gmail.com>
2016-10-01 17:40:36 -0400

    1.3 and 1.4 seem to be working

--- code/computePrincipalCurvature.m
+++ code/computePrincipalCurvature.m
@@ -17,27 +17,21 @@ for i = 1:nz
     [Dx,Dy] = gradient(DoGPyramid(:,:,i));
     [Dxx,Dxy] = gradient(Dx);
     [Dyx,Dyy] = gradient(Dy);
-    
+        
     hessians = Dxx;
     hessians(:,:,2) = Dyx;
     hessians(:,:,3) = Dxy;
     hessians(:,:,4) = Dyy;
-    
-%     temp = [Dxx(2,1) Dxy(2,1);
-%             Dyx(2,1) Dyy(2,1)]
-    
+
     hessians = permute(hessians,[3 1 2]);
     hessians = reshape(hessians,2,[],1);
     hessians = reshape(hessians,2,2,[]);
-    
-%     hessians(:,:,2)
-    
+     
     for j= 1:nx*ny
         H = hessians(:,:,j);
-        PrincipalCurvature(j,i) = trace(H)^2/det(H);
+        PrincipalCurvature(j,i) = trace(H).^2./det(H);
     end
 end
-size(PrincipalCurvature)
 PrincipalCurvature = reshape(PrincipalCurvature,nx,ny,nz);
-% PrincipalCurvature = PrincipalCurvature(PrincipalCurvature > thetar);
+
  
