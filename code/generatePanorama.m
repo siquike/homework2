534e15ddb6ad543fd90ae88b156eb9e6765ca81d
Luis <siquike@gmail.com>
2016-10-12 23:34:31 -0400

    finished

--- code/generatePanorama.m
+++ code/generatePanorama.m
@@ -2,10 +2,10 @@ function im3 = generatePanorama(im1,im2)
 [locs1, desc1] = briefLite(im1);
 [locs2, desc2] = briefLite(im2);
 [matches] = briefMatch(desc1, desc2,0.3);
-% [bestH] = ransacH(matches, locs1, locs2, 20000, 1);
-% [im3] = imageStitching_noClip(im1, im2, bestH);
-match1 = locs1(matches(:,1),1:2);
-match2 = locs2(matches(:,2),1:2);
-H2to1 = computeH(match1',match2');
-[im3] = imageStitching(im1, im2, H2to1);
+[bestH] = ransacH(matches, locs1, locs2, 20000, 1);
+[im3] = imageStitching_noClip(im1, im2, bestH);
+% match1 = locs1(matches(:,1),1:2);
+% match2 = locs2(matches(:,2),1:2);
+% H2to1 = computeH(match1',match2');
+% [im3] = imageStitching(im1, im2, H2to1);
 end
